e31ea8ccfd5950a99f2eb3e7bc09c9c8e9840bcc
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-07-29 19:58:04 +0530

    final commit

--- src/components/MainDash.jsx
+++ src/components/MainDash.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DashHead from "../components/DashHead"
 import Projects from '../pages/Projects'
 import Overview from './Overview'
